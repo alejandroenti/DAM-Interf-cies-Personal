e3e6ee390a97a0ad4abe38e83dabd73e736cb786
Alejandro López <116338818+alejandroenti@users.noreply.github.com>
2025-09-29 22:53:25 +0200

    Eliminat import que no es fa servir

--- JavaFX/Exercici02/src/main/java/com/project/ControllerDesktop.java
+++ JavaFX/Exercici02/src/main/java/com/project/ControllerDesktop.java
@@ -21,7 +21,6 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 
 public class ControllerDesktop implements Initializable {
 
